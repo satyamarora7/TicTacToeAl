579aa8433bcb8dd8cb4af8835f90b57afe2091d9
sat0004 <satyam13arora1999@gmail.com>
2019-09-10 03:20:00 +0530

    Added board layout

--- app/src/main/java/com/example/tictactoeusingminimaxalgorithm/MainActivity.kt
+++ app/src/main/java/com/example/tictactoeusingminimaxalgorithm/MainActivity.kt
@@ -2,11 +2,42 @@ package com.example.tictactoeusingminimaxalgorithm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    //Creating a 2D Array of ImageViews
+    private val boardCells = Array(3) { arrayOfNulls<ImageView>(3) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //calling the function to load out tic tac toe board
+        loadBoard()
+    }
+
+    // This function is generating the tic tac toe board
+    private fun loadBoard() {
+        for(i in boardCells.indices) {
+            for(j in boardCells.indices) {
+                boardCells[i][j] = ImageView(this)
+                boardCells[i][j]?.layoutParams = GridLayout.LayoutParams().apply {
+                    rowSpec = GridLayout.spec(i)
+                    columnSpec = GridLayout.spec(j)
+                    width = 250
+                    height = 250
+                    bottomMargin = 5
+                    topMargin = 5
+                    leftMargin = 5
+                    rightMargin = 5
+                }
+                boardCells[i][j]?.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary))
+                layout_board.addView(boardCells[i][j])
+            }
+        }
     }
 }
